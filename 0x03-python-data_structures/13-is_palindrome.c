65fafb3835ca2831811925f5ccf05a9b4b06ef63
David <mureithidave@gmail.com>
2024-03-19 06:56:38 -0700

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,10 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-
-int main(void)
+int is_palindrome(listint_t **head)
 {
-	listint_t *head;
+	listint_t *cur = *head;
+	int tab[2048], i = 0, j = 0;
 
-	return (0);
+	if (*head)
+	{
+		while (cur)
+		{
+			tab[i] = cur->n;
+			cur = cur->next;
+			i++;
+		}
+
+		while (j < i / 2)
+		{
+			if (tab[j] == tab[i - j - 1])
+				j++;
+			else
+				return (0);
+		}
+	}
+	return (1);
 }
